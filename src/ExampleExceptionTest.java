64fdf7d686131a89efcb051c09357f12a3c9ca0f
Kateryna Borysova <katarina.bk@ukr.net>
2019-07-07 21:44:26 +0300

    tests written

--- src/ExampleExceptionTest.java
+++ src/ExampleExceptionTest.java
@@ -6,42 +6,46 @@ import org.testng.annotations.Test;
 
 public class ExampleExceptionTest {
 
-  /**
-   * Test data for positive test.
-   *
-   * @return test data array
-   */
-  @DataProvider(name = "data")
-  public static Object[][] data() {
-    return new Object[][]{
-        {2, 2, 4},
-        {2, 3, 6}
-        // TODO add 2 more test data here
-    };
-  }
+    /**
+     * Test data for positive test.
+     *
+     * @return test data array
+     */
+    @DataProvider(name = "data")
+    public static Object[][] data() {
+        return new Object[][]{
+                {2, 2, 4},
+                {2, 3, 6},
+                {0, 18, 0},
+                {12, 2, 24}
+        };
+    }
 
-  /**
-   * Test data for exception test.
-   *
-   * @return test data array
-   */
-  @DataProvider(name = "negativeData")
-  public static Object[][] negativeData() {
-    return new Object[][]{
-        {-2, 2},
-        {2, -2}
-        // TODO add 2 more test data here
-    };
-  }
+    /**
+     * Test data for exception test.
+     *
+     * @return test data array
+     */
+    @DataProvider(name = "negativeData")
+    public static Object[][] negativeData() {
+        return new Object[][]{
+                {-2, 2},
+                {2, -2},
+                {-4, -3},
+                {-1, 0}
+        };
+    }
 
-  @Test(dataProvider = "data")
-  public void testRectangleArea(int a, int b, int c) {
-    // TODO put your code here
-  }
+    @Test(dataProvider = "data")
+    public void testRectangleArea(int a, int b, int c) {
+        assertEquals(ExampleException.rectangleArea(a, b), c);
+    }
 
 
-  @Test(dataProvider = "negativeData")
-  public void testRectangleAreaNegative(int a, int b) {
-    // TODO put your code here
-  }
+    @Test(dataProvider = "negativeData",
+            expectedExceptions = IllegalArgumentException.class,
+            expectedExceptionsMessageRegExp = "input value is below zero!")
+    public void testRectangleAreaNegative(int a, int b) {
+        ExampleException.rectangleArea(a, b);
+    }
 }
